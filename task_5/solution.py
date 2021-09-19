97c5b43e3a1fab8f74956d1099c7b223a1c60c44
Антипов Даниил <72620861+CHAMPMOON@users.noreply.github.com>
2021-09-19 14:15:00 +0300

    Update solution.py

--- task_5/solution.py
+++ task_5/solution.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+
+data = pd.read_csv('train.csv')
+data_bool = data.isnull()
+for col in data.columns:
+    for i in range(data[col].size):
+        if data_bool[col][i] == True:
+            if i == 0:
+                j = i + 1
+                while True:
+                    if data_bool[col][j] == False:
+                        break
+                    j += 1
+                data[col][i] = data[col][j]
+            else: 
+                data[col][i] = data[col][i - 1]
+print(data.isnull())
