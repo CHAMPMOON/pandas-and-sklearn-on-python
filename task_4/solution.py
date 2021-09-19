90d10019e6603408b8fca651ca1428a966d26be1
Антипов Даниил <72620861+CHAMPMOON@users.noreply.github.com>
2021-09-19 14:10:46 +0300

    Update solution.py

--- task_4/solution.py
+++ task_4/solution.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+
+data = pd.read_csv('train.csv')
+data_name = data['Name']
+dc = {}
+for i in range(data_name.size):
+    data_name[i] = data_name[i].split(',')[0]
+    if data_name[i] not in dc:
+        dc[data_name[i]] = 1
+    else: dc[data_name[i]] += 1
+list_dc = list(dc.items())
+list_dc.sort(key=lambda i: -i[1])
+print("\n10 popular name:")
+for i in range(10):
+    print(list_dc[i][0], '-', list_dc[i][1])
