bf97f4a6c594a5ff705b0dd57f62646d65088abc
Антипов Даниил <72620861+CHAMPMOON@users.noreply.github.com>
2021-09-19 13:59:13 +0300

    Update solution.py

--- task_2/solution.py
+++ task_2/solution.py
@@ -4,4 +4,5 @@ import pandas as pd
 data = pd.read_csv('train.csv')
 data_male = data[data.Sex == "male"][['PassengerId', 'Survived', 'Pclass', 'Age', 'SibSp', 'Fare']]
 data_female = data[data.Sex == "female"][['PassengerId', 'Survived', 'Pclass', 'Age', 'SibSp', 'Fare']]
-print(data_male, data_female, sep="\n")
+print("\tMALE:", data_male, sep="\n")
+print("\n\tFEMALE:", data_female, sep="\n")
