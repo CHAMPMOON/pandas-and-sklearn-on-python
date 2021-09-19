993a083568aa35c477cb781e16a905e0084b144a
Антипов Даниил <72620861+CHAMPMOON@users.noreply.github.com>
2021-09-19 13:52:57 +0300

    Update solution.py

--- task_2/solution.py
+++ task_2/solution.py
@@ -1,1 +1,7 @@
+import pandas as pd  
 
+
+data = pd.read_csv('train.csv')
+data_male = data[data.Sex == "male"][['PassengerId', 'Survived', 'Pclass', 'Age', 'SibSp', 'Fare']]
+data_female = data[data.Sex == "female"][['PassengerId', 'Survived', 'Pclass', 'Age', 'SibSp', 'Fare']]
+print(data_male, data_female, sep="\n")
